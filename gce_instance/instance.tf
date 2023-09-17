8baf412c2fa577935d24e74e33d4156c0d383f48
Christopher Hoskin <christopher.hoskin@overleaf.com>
2023-09-17 20:31:13 +0100

    Zone

--- gce_instance/instance.tf
+++ gce_instance/instance.tf
@@ -1,7 +1,8 @@
 resource "google_compute_instance" "overleaf" {
   name         = "overleaf"
   machine_type = "e2-medium"
-  project = var.project_id
+  project      = var.project_id
+  zone         = var.zone
 
   network_interface {
     network = "default"
--- gce_instance/variables.tf
+++ gce_instance/variables.tf
@@ -1,3 +1,8 @@
 variable "project_id" {
   type = string
 }
+
+variable "zone" {
+  type    = string
+  default = "us-east1-b"
+}
