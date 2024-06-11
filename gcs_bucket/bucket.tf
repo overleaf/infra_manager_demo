92aec5dae434371c24eef456401fb868092c533c
Christopher Hoskin <christopher.hoskin@overleaf.com>
2024-06-11 11:46:41 +0100

    Add versioning and lifecycle

--- gcs_bucket/bucket.tf
+++ gcs_bucket/bucket.tf
@@ -1,4 +1,19 @@
 resource "google_storage_bucket" "csh-2024-06-11-lifecycle-test" {
   name          = "csh-2024-06-11-lifecycle-test"
   location      = "us-east1"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type          = "Delete"
+      storage_class = null
+    }
+    condition {
+      days_since_noncurrent_time = 90
+    }
+  }
+
+
 }
